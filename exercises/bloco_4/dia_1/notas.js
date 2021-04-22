2de02999fd6734ed33296ef86586981c53802e7d
Roberval Filho <rslfilho@gmail.com>
2021-04-22 18:20:39 -0300

    7 Notas

---
+++ exercises/bloco_4/dia_1/notas.js
@@ -0,0 +1,20 @@
+let score = 95;
+
+if(score >= 90) {
+    console.log('A');
+}
+else if(score >= 80) {
+    console.log('B');
+}
+else if(score >= 70) {
+    console.log('C');
+}
+else if(score >= 60) {
+    console.log('D');
+}
+else if(score >= 50) {
+    console.log('E');
+}
+else if(score < 50) {
+    console.log('F');
+}

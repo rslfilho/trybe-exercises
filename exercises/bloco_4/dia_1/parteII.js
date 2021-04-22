a5a1f36e89b38279f0b336e36d31fb948c3fc06b
Roberval Filho <rslfilho@gmail.com>
2021-04-22 16:21:04 -0300

    Exercícios durante as aulas Partes II, III, IV e V

---
+++ exercises/bloco_4/dia_1/parteII.js
@@ -0,0 +1,17 @@
+const name = "Roberval";
+const birthCity = "Teresina";
+let birthYear = 1989;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Fortaleza";
+
+console.log(birthCity);
+
+// Erro pois houve uma tentativa de atribuição de novo valor a uma constante.
---
+++ exercises/bloco_4/dia_1/parteIII.js
@@ -0,0 +1,29 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+// console.log(typeof patientAge);
+
+// patientId = '50';
+
+// console.log(typeof patientId);
+
+let base = 5;
+let altura = 8;
+
+let area = altura * base;
+let perimetro = 2 * base + 2 * altura;
+
+console.log(base);
+console.log(altura);
+console.log(area);
+console.log(perimetro);

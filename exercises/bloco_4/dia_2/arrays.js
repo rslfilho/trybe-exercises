c600bc53ebcd6123bb73477890013024ca248db7
Roberval Filho <rslfilho@gmail.com>
2021-04-23 18:15:09 -0300

    Exercícios durante as aulas Arrays

--- exercises/bloco_4/dia_2/arrays.js
+++ exercises/bloco_4/dia_2/arrays.js
@@ -2,3 +2,36 @@ let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
 console.log(tasksList.length);
 // 3
+
+tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
+tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+tasksList.unshift('Enfiando no Começo');
+console.log(tasksList);
+
+tasksList.pop();
+console.log(tasksList);
+
+tasksList.shift();
+console.log(tasksList);
+
+tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);
+
+// 1
---
+++ exercises/bloco_4/dia_2/exercicio_arrays.js
@@ -0,0 +1,21 @@
+// Exercício 01
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Exercício 02
+
+menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// Exercício 03
+
+menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato');
+
+console.log(menu);

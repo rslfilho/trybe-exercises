1a93a5993e66816ea634120a0fe5e0164cabcc8b
Roberval Filho <rslfilho@gmail.com>
2021-05-12 16:44:16 -0300

    Mudando a forma de linkar o bootstrap

--- exercises/bloco_6/dia_2/script.js
+++ exercises/bloco_6/dia_2/script.js
@@ -39,200 +39,200 @@ function addStates() {
   }
 }
 
-function createErrorSection() {
-  const parentElement = document.querySelector('main');
-  const section = document.createElement('section');
+// function createErrorSection() {
+//   const parentElement = document.querySelector('main');
+//   const section = document.createElement('section');
 
-  section.id = 'error';
-  section.innerHTML = '<fieldset id="errorInfo"><legend>Algo deu Errado</legend></fieldset>';
+//   section.id = 'error';
+//   section.innerHTML = '<fieldset id="errorInfo"><legend>Algo deu Errado</legend></fieldset>';
 
-  parentElement.insertBefore(section, parentElement.firstElementChild);
-}
+//   parentElement.insertBefore(section, parentElement.firstElementChild);
+// }
 
-function createErrorP(typeOfError) {
-  const parentElement = document.getElementById('errorInfo');
-  const errorP = document.createElement('p');
+// function createErrorP(typeOfError) {
+//   const parentElement = document.getElementById('errorInfo');
+//   const errorP = document.createElement('p');
 
-  if (typeOfError === 'completion') {
-    errorP.innerHTML = 'Todos os campos são Obrigatórios!';
-  } else if (typeOfError === 'invalidDate') {
-    errorP.innerHTML = 'Data Inválida!';
-  }
+//   if (typeOfError === 'completion') {
+//     errorP.innerHTML = 'Todos os campos são Obrigatórios!';
+//   } else if (typeOfError === 'invalidDate') {
+//     errorP.innerHTML = 'Data Inválida!';
+//   }
 
-  parentElement.appendChild(errorP);
-}
+//   parentElement.appendChild(errorP);
+// }
 
-function checkCompletion() {
-  const inputs = document.getElementsByTagName('input');
-  const textAreas = document.getElementsByTagName('textarea');
+// function checkCompletion() {
+//   const inputs = document.getElementsByTagName('input');
+//   const textAreas = document.getElementsByTagName('textarea');
 
-  for (let index = 0; index < inputs.length; index += 1) {
-    if (inputs[index].value === '') {
-      return createErrorSection(), createErrorP('completion');
-    } 
-  }
+//   for (let index = 0; index < inputs.length; index += 1) {
+//     if (inputs[index].value === '') {
+//       return createErrorSection(), createErrorP('completion');
+//     } 
+//   }
 
-  for (let index = 0; index < textAreas.length; index += 1) {
-    if (textAreas[index].value === '') {
-      return createErrorSection(), createErrorP('completion');
-    } 
-  }
-
-  return 'go';
-}
-
-// function checkMaxLength() {
-//   if (document.getElementById('name').value.length > 40) alert('Nome muito grande');
-//   if (document.getElementById('email').value.length > 50) alert('Email muito grande');
-//   if (document.getElementById('cpf').value.length > 11) alert('CPF muito grande');
-//   if (document.getElementById('adress').value.length > 200) alert('Endereço muito grande');
-//   if (document.getElementById('city').value.length > 28) alert('Cidade muito grande');
-//   if (document.getElementById('resume').value.length > 1000) alert('Resumo muito grande');
-//   if (document.getElementById('office-description').value.length > 500) alert('Descrição muito grande');
+//   for (let index = 0; index < textAreas.length; index += 1) {
+//     if (textAreas[index].value === '') {
+//       return createErrorSection(), createErrorP('completion');
+//     } 
+//   }
 
 //   return 'go';
 // }
 
-function checkDate() {
-  const dateInput = document.getElementById('initial-date');
-  const datePattern = /^\d\d\/\d\d\/\d\d\d\d$/;
+// // function checkMaxLength() {
+// //   if (document.getElementById('name').value.length > 40) alert('Nome muito grande');
+// //   if (document.getElementById('email').value.length > 50) alert('Email muito grande');
+// //   if (document.getElementById('cpf').value.length > 11) alert('CPF muito grande');
+// //   if (document.getElementById('adress').value.length > 200) alert('Endereço muito grande');
+// //   if (document.getElementById('city').value.length > 28) alert('Cidade muito grande');
+// //   if (document.getElementById('resume').value.length > 1000) alert('Resumo muito grande');
+// //   if (document.getElementById('office-description').value.length > 500) alert('Descrição muito grande');
+
+// //   return 'go';
+// // }
+
+// function checkDate() {
+//   const dateInput = document.getElementById('initial-date');
+//   const datePattern = /^\d\d\/\d\d\/\d\d\d\d$/;
   
-  if (datePattern.test(dateInput.value) === false) {
-    return createErrorSection(), createErrorP('invalidDate');
-  }
-  return 'go';
-}
+//   if (datePattern.test(dateInput.value) === false) {
+//     return createErrorSection(), createErrorP('invalidDate');
+//   }
+//   return 'go';
+// }
 
-function createCvSection() {
-  const parentElement = document.querySelector('main');
-  const section = document.createElement('section');
+// function createCvSection() {
+//   const parentElement = document.querySelector('main');
+//   const section = document.createElement('section');
 
-  section.id = 'curriculum';
-  section.innerHTML = '<h2>Seu Currículo</h2><fieldset id="personal"><legend>Dados Pessoais</legend></fieldset><fieldset id="last-job"><legend>Dados do último Emprego</legend></fieldset>';
+//   section.id = 'curriculum';
+//   section.innerHTML = '<h2>Seu Currículo</h2><fieldset id="personal"><legend>Dados Pessoais</legend></fieldset><fieldset id="last-job"><legend>Dados do último Emprego</legend></fieldset>';
 
-  parentElement.insertBefore(section, parentElement.firstElementChild);
-}
+//   parentElement.insertBefore(section, parentElement.firstElementChild);
+// }
 
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
-}
+// function onlyUnique(value, index, self) {
+//   return self.indexOf(value) === index;
+// }
 
-function createNameArray() {
-  const form = document.forms[0];
-  const allNames = [];
+// function createNameArray() {
+//   const form = document.forms[0];
+//   const allNames = [];
 
-  for (let index = 0; index < form.length; index += 1) {
-    allNames[index] = form.elements[index].name;
-  }
+//   for (let index = 0; index < form.length; index += 1) {
+//     allNames[index] = form.elements[index].name;
+//   }
 
-  const noEmptyNames = allNames.filter( (el) => { return el; });
-  return noEmptyNames.filter(onlyUnique);
-}
+//   const noEmptyNames = allNames.filter( (el) => { return el; });
+//   return noEmptyNames.filter(onlyUnique);
+// }
 
-function createNameP(inputIndex) {
-  const inputNames = createNameArray();
-  const personalData = document.getElementById('personal');
-  const lastJobData = document.getElementById('last-job');
-  const dataField = document.createElement('p');
+// function createNameP(inputIndex) {
+//   const inputNames = createNameArray();
+//   const personalData = document.getElementById('personal');
+//   const lastJobData = document.getElementById('last-job');
+//   const dataField = document.createElement('p');
   
-  dataField.innerHTML = document.forms[0].elements[inputNames[inputIndex]].value
+//   dataField.innerHTML = document.forms[0].elements[inputNames[inputIndex]].value
   
-  if (inputIndex < 7) {
-    personalData.appendChild(dataField);
-  } else {
-    lastJobData.appendChild(dataField);
-  }
+//   if (inputIndex < 7) {
+//     personalData.appendChild(dataField);
+//   } else {
+//     lastJobData.appendChild(dataField);
+//   }
   
-}
+// }
 
-function createLabelArray() {
-  const labels = document.querySelectorAll('label');
-  const allLabels = [];
+// function createLabelArray() {
+//   const labels = document.querySelectorAll('label');
+//   const allLabels = [];
 
-  for (let index = 0; index < labels.length; index += 1) {
-    if (index === 6) {
-      allLabels.push('Tipo:');
-      index += 2;
-    }
+//   for (let index = 0; index < labels.length; index += 1) {
+//     if (index === 6) {
+//       allLabels.push('Tipo:');
+//       index += 2;
+//     }
 
-    allLabels.push(labels[index].innerHTML);
-  }
+//     allLabels.push(labels[index].innerHTML);
+//   }
 
-  return allLabels;
-}
+//   return allLabels;
+// }
 
-function createLabel(labelIndex) {
-  const labelContent = createLabelArray();
-  const personalData = document.getElementById('personal');
-  const lastJobData = document.getElementById('last-job');
-  const labelField = document.createElement('label');
+// function createLabel(labelIndex) {
+//   const labelContent = createLabelArray();
+//   const personalData = document.getElementById('personal');
+//   const lastJobData = document.getElementById('last-job');
+//   const labelField = document.createElement('label');
 
-  labelField.innerHTML = labelContent[labelIndex];
+//   labelField.innerHTML = labelContent[labelIndex];
   
-  if (labelIndex < 7) {
-    personalData.appendChild(labelField);
-  } else {
-    lastJobData.appendChild(labelField);
-  }
+//   if (labelIndex < 7) {
+//     personalData.appendChild(labelField);
+//   } else {
+//     lastJobData.appendChild(labelField);
+//   }
   
-}
+// }
 
-function createCurriculum() {
-  createCvSection();
+// function createCurriculum() {
+//   createCvSection();
   
-  for (let index = 0; index < 11; index += 1) {
-    createLabel(index);
-    createNameP(index);
-  }
+//   for (let index = 0; index < 11; index += 1) {
+//     createLabel(index);
+//     createNameP(index);
+//   }
   
-}
+// }
 
-function removeCreatedElement() {
-  const parentElement = document.querySelector('main');
-  const formElement = document.querySelector('#form');
+// function removeCreatedElement() {
+//   const parentElement = document.querySelector('main');
+//   const formElement = document.querySelector('#form');
   
-  if (formElement !== parentElement.firstElementChild) {
-    parentElement.removeChild(parentElement.firstElementChild);
-  }
-}
+//   if (formElement !== parentElement.firstElementChild) {
+//     parentElement.removeChild(parentElement.firstElementChild);
+//   }
+// }
 
-function eventSubmitForm() {
-  const button = document.getElementById('submit');
+// function eventSubmitForm() {
+//   const button = document.getElementById('submit');
   
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    removeCreatedElement();
+//   button.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     removeCreatedElement();
 
-    if (checkCompletion() === 'go' && checkDate() === 'go') {
-      createCurriculum();
-      location.href = '#curriculum';
-    } else {
-      location.href = '#error';
-    }
-    // checkCompletion();
-    // // checkMaxLength();
-    // checkDate();
-  })
-}
+//     if (checkCompletion() === 'go' && checkDate() === 'go') {
+//       createCurriculum();
+//       location.href = '#curriculum';
+//     } else {
+//       location.href = '#error';
+//     }
+//     // checkCompletion();
+//     // // checkMaxLength();
+//     // checkDate();
+//   })
+// }
 
-function eventClearButton() {
-  const clearButton = document.getElementById('clear');
-  const inputs = document.querySelectorAll('input');
-  const textarea = document.querySelectorAll('textarea');
+// function eventClearButton() {
+//   const clearButton = document.getElementById('clear');
+//   const inputs = document.querySelectorAll('input');
+//   const textarea = document.querySelectorAll('textarea');
 
-  clearButton.addEventListener('click', () => {
-    removeCreatedElement();
+//   clearButton.addEventListener('click', () => {
+//     removeCreatedElement();
 
-    for (let indexInputs = 0; indexInputs < inputs.length; indexInputs += 1) {
-      inputs[indexInputs].value = '';
-    }
+//     for (let indexInputs = 0; indexInputs < inputs.length; indexInputs += 1) {
+//       inputs[indexInputs].value = '';
+//     }
 
-    for (let indexTextArea = 0; indexTextArea < textarea.length; indexTextArea += 1) {
-      textarea[indexTextArea].value = '';
-    }
-  })
-}
+//     for (let indexTextArea = 0; indexTextArea < textarea.length; indexTextArea += 1) {
+//       textarea[indexTextArea].value = '';
+//     }
+//   })
+// }
 
 addStates();
-eventSubmitForm();
-eventClearButton();
+// eventSubmitForm();
+// eventClearButton();

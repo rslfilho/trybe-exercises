871baad9a550ac33a4fe26e30aeaccd9b5b3b1aa
Roberval Filho <rslfilho@gmail.com>
2021-05-28 19:05:21 -0300

    Ecercicio 2 atualizado

--- exercises/bloco_8/dia_4/exercicio_2.js
+++ exercises/bloco_8/dia_4/exercicio_2.js
@@ -1,9 +1,8 @@
 const assert = require('assert');
 const books = require('./books');
 
-const expectedResult = 43;
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
-const averageAge = () => books.reduce((acc, book) => acc.concat(book.author.name), []).join(', ')
+const reduceNames = () => books.reduce((acc, book) => acc.concat(book.author.name), []).join(', ') + '.';
 
-console.log(averageAge())
-// assert.strictEqual(averageAge(), expectedResult);
+assert.strictEqual(reduceNames(), expectedResult);
